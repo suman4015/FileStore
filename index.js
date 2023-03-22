4ee431db1ae588b262b2bf2e061af881d8960b79
suman4015 <60983585+suman4015@users.noreply.github.com>
2023-03-22 17:15:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Hello World!");
 }); 
-
+var data = {
+    fullName : name,
+    phone : phone,
+    email : email,
+    desc : desc
+};
 function printMsg(){
 	window.alert('Hello There');
+	$.ajax({
+    	type: "POST",
+    	URL : 'https://pl3q36s3g3.execute-api.ap-south-1.amazonaws.com/default/ajaxLambdaFun',
+    	crossDomain: "true",
+    	contentType: "application/json",
+    	data: JSON.stringify(data),
+    	success: function () {
+         alert("Successful!");
+    	},
+    	error: function () {
+         	alert("Unsuccessful!");
+    	}});
+});
 }
